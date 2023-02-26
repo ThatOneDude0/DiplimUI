79fa328407cae8ab6b809f6a3533bb849d949150
ThatOneDude0 <99406502+ThatOneDude0@users.noreply.github.com>
2023-02-26 19:25:08 +0300

    Load
    l

---
+++ Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject _canvasLoadScreen;
+    [SerializeField] private Slider _sliderProgress;
+
+    public void Start()
+    {
+        //_canvasLoadScreen.SetActive(true);
+
+        StartCoroutine(LoadAsync());
+    }
+
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(1);
+        asyncOperation.allowSceneActivation = false;
+
+        while (!asyncOperation.isDone)
+        {
+            _sliderProgress.value = asyncOperation.progress;
+
+            if (asyncOperation.progress >= .9f && !asyncOperation.allowSceneActivation)
+            {
+                yield return new WaitForSeconds(2.3f);
+                asyncOperation.allowSceneActivation = true;
+            }
+        }
+
+        yield return null;
+    }
+}
